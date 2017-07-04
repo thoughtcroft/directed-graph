2a6b2c45e1bfd98d50d25528770dfa4187957c0a
Warren Bain <warren@thoughtcroft.com>
2017-07-04 18:08:08 +1000

    Can construct a directed graph

---
+++ glow_navigator.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint: disable=redefined-builtin
+
+"""Glow Navigator
+
+Command line tool for exloring the relationships between
+Form-flows and Templates in Glow designed apps
+"""
+
+# python2 and python3 portability
+from __future__ import print_function
+from builtins import input
+
+# standard libraries
+import glob
+import pdb
+import re
+import xml.etree.ElementTree as ET
+import yaml
+
+# external libraries
+import networkx as nx
+
+GLOW_OBJECTS = {
+    "Formflow": {
+        "path": "BPMWorkflowTmpl/*.yaml",
+        "type": "Formflow",
+        "fields": {
+            "guid": "VM_PK",
+            "name": "VM_Name",
+            "is_active": "VM_IsActive",
+            "entity": "VM_EntityType",
+            "tasks": "BPMTaskTmpls"
+            }
+        },
+    "Template": {
+        "path": "BPMForm/*.yaml",
+        "type": "Template",
+        "fields": {
+            "guid": "VZ_PK",
+            "name": "VZ_FormID",
+            "is_active": "VZ_IsActive",
+            "entity": "VZ_EntityType",
+            "data": "VZ_FormData"
+            }
+        },
+    "Task": {
+        "type": "Task",
+        "fields": {
+            "guid": "VR_PK",
+            "name": "VR_Description",
+            "is_active": "VR_IsActive",
+            "task_type": "VR_Type",
+            "template": "VR_VZ_Form",
+            "formflow": "VR_VM_JumpToWorkflowTemplate"
+            }
+        }
+    }
+
+
+class GlowObject(object):
+    # pylint: disable=too-few-public-methods
+    """Glow Object class
+
+    Represents various objects available in Glow.
+    A field mapping provides a friendly and object
+    neutral way to access various values passed
+    in from the YAML definition
+    """
+    def __init__(self, mapping, values):
+        self.type = mapping["type"]
+        self.fields = mapping["fields"]
+        self.values = values
+
+    def __getattr__(self, attr):
+        """Provide value lookup as attributes
+
+        Allows value dict items to be accessed as
+        properties e.g. {"foo": "bar"} can be
+        accessed as my_obj.foo.
+
+        Also uses the friednly mapping such that
+        if "foo" is mapped to "baz" then we can
+        call my_obj.baz to obtain "bar"
+        """
+        field = attr
+        if attr in self.fields.keys():
+            field = self.fields[attr]
+        try:
+            return self.values[field]
+        except KeyError:
+            return None
+
+    def map(self):
+        """Return a dict of values mapped to fields
+
+        Returns a limited subset of values: those that
+        have keys mapped in the fields dictionary
+        """
+        return {k : self.values[v]
+                for k, v in self.fields.iteritems()
+                if v in self.values.keys()}
+
+
+class XMLParser(object):
+    # pylint: disable=too-few-public-methods
+    """Glow Template XML parser
+    """
+    TOPICS = {
+        "Text": "name",
+        "Description": "description",
+        "PagePK": "template",
+        "Workflow": "formflow"
+        }
+
+    def __init__(self, xml):
+        self.tree = ET.fromstring(xml)
+
+    def iterfind(self, tag):
+        """Generator for elements matching tag
+
+        Find nodes with tag then burrow into its
+        collection of Placeholder elements and return
+        the name, value pairs as a dictionary
+        """
+        for node in self.tree.iter():
+            if remove_xmlns(node.tag) == tag:
+                yield self._ph_dict(node)
+
+    def _ph_dict(self, node):
+        """Create the dictionary of Placeholders
+
+        The nodes Placeholder element attributes
+        are remapped using the topics dict. Each
+        {name: topic, value: amount} is turned into
+        {topic: amount} if value has been set
+        """
+        ph_dict = {}
+        for elem in node.iter():
+            if remove_xmlns(elem.tag) != "Placeholder":
+                continue
+            e_dict = elem.attrib
+            if "ResKey" in e_dict.keys():
+                ph_dict["guid"] = e_dict["ResKey"]
+            if e_dict["Value"] and e_dict["Name"] in self.TOPICS.keys():
+                key = self.TOPICS[e_dict["Name"]]
+                ph_dict[key] = e_dict["Value"]
+        return ph_dict
+
+
+def raw_guid(guid):
+    """Remove hyphens from string guid
+    """
+    return guid.replace("-", "")
+
+def load_file(file_name):
+    """Return YAML from required file
+    """
+    return yaml.load(open(file_name, "r"))
+
+def remove_xmlns(text):
+    """Strip out any xmlns from xml tag
+    """
+    return re.sub(r"\{.*\}", "", text)
+
+def glow_file_objects():
+    """Return the glow objects with files
+    """
+    return (v for _, v in GLOW_OBJECTS.iteritems()
+            if "path" in v.keys())
+
+
+def main():
+    """Create directed graph of objects
+    """
+    graph = nx.DiGraph(name="Glow")
+    for attrs in glow_file_objects():
+        for file_name in glob.iglob(attrs["path"]):
+            values = load_file(file_name)
+            glow_object = GlowObject(attrs, values)
+            graph.add_node(glow_object.guid, glow_object.map())
+
+            if glow_object.type == "Formflow" and glow_object.tasks:
+                for task in glow_object.tasks:
+                    go_task = GlowObject(GLOW_OBJECTS["Task"], task)
+                    if go_task.task_type == "FRM":
+                        graph.add_edge(glow_object.guid, go_task.template, go_task.map())
+                    elif go_task.task_type == "JMP":
+                        graph.add_edge(glow_object.guid, go_task.formflow, go_task.map())
+                continue
+
+            if glow_object.type == "Template" and glow_object.data:
+                xml_parser = XMLParser(glow_object.data)
+                for tile in xml_parser.iterfind("Tile"):
+                    if "template" in tile.keys():
+                        graph.add_edge(glow_object.guid, tile["template"], tile)
+                    elif "formflow" in tile.keys():
+                        graph.add_edge(glow_object.guid, tile["formflow"], tile)
+                continue
+
+
+    # print our graph
+    print(nx.info(graph))
+    pdb.set_trace()
+
+if __name__ == "__main__":
+    main()
---
+++ test_glow_navigator.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+from ddt import ddt, data, unpack
+
+from glow_navigator import *
+
+
+class YAMLTestCase(unittest.TestCase):
+    """Set up and tear down for the YAML files"""
+
+    def setUp(self):
+        self.formflow_data = load_file("test_data/test_formflow.yaml")
+        self.formflow = GlowObject(GLOW_OBJECTS["Formflow"], self.formflow_data)
+        self.template_data = load_file("test_data/test_template.yaml")
+        self.template = GlowObject(GLOW_OBJECTS["Template"], self.template_data)
+
+    def tearDown(self):
+        self.formflow_data = None
+        self.formflow = None
+        self.template_data = None
+        self.template = None
+
+@ddt
+class BaseTestCase(YAMLTestCase):
+    """Unit tests for general functions"""
+
+    @data(("1-2-3-4-5", "12345"), ("abc", "abc"), ("ab c5 / 0", "ab c5 / 0"))
+    @unpack
+    def test_raw_guid(self, first, second):
+        """Converts string guid to plain form without hyphens"""
+        self.assertEqual(raw_guid(first), second)
+
+    @data(("guid", "tic-tac-toe"), ("name", "My Test Template"),
+          ("type", "Template"), ("bar", None),
+          ("entity", "My Test Entity"), ("is_active", False))
+    @unpack
+    def test_template_attribs(self, first, second):
+        """Retrieves an attribute from parsed yaml"""
+        value = getattr(self.template, first)
+        self.assertEqual(value, second)
+
+    @data(("type", "Formflow"), ("guid", "foo-bar-baz"),
+          ("name", "My Test Formflow"), ("foo", None),
+          ("entity", "My Test Entity"), ("is_active", True))
+    @unpack
+    def test_formflow_attribs(self, first, second):
+        """Retrieves an attribute from parsed yaml"""
+        value = getattr(self.formflow, first)
+        self.assertEqual(value, second)
+
+    def test_value_map(self):
+        """Returns a dictionary of mapped values"""
+        test_map = {
+            "guid": "foo-bar-baz",
+            "name": "My Test Formflow",
+            "entity": "My Test Entity",
+            "is_active": True
+            }
+        self.assertEqual(sorted(self.formflow.map()), sorted(test_map))
+
+@ddt
+class NonYAMLTestCase(unittest.TestCase):
+    """Unit tests for code not dependent on YAML"""
+
+    @data(("{http://schemas.microsoft.com/winfx/}UserControl", "UserControl"),
+          ("no name space here", "no name space here"))
+    @unpack
+    def test_namespace_remover(self, first, second):
+        """Extracts the xml namespace from a string"""
+        self.assertEqual(remove_xmlns(first), second)
+if __name__ == '__main__':
+    unittest.main()
